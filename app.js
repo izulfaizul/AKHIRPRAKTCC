a202690393e4b0eafc3364e1cbaf4189a9d36505
Usamah Faizul <faizulislam2001@gmail.com>
2023-05-29 19:41:32 +0700

    Menambah dependenci dotenv dan cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const userRouter = require('./app/user/route');
 const courseRouter = require('./app/course.js/route');
 const customErrorHandler = require('./middleware/customErrorHandler');
 const page404NotFound = require('./middleware/handler404NotFound');
 var app = express();
-
+require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,14 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", 'true');
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-Credentials", 'true');
+//     next();
+//   });
 
 app.use('/user', userRouter);
 app.use('/course', courseRouter);
